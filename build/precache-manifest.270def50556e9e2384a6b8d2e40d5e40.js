a2e109951324d6e21f2836bf3b7473d1e85e6214
Norberto Caceres <norbe1994@yahoo.com>
2019-03-18 14:23:21 -0500

    Build for production

---
+++ build/precache-manifest.270def50556e9e2384a6b8d2e40d5e40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c0436d55f12af99760ca65640d19d19",
+    "url": "/static/media/logo.5c0436d5.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ecf3a6be9e1ec0f8bce",
+    "url": "/static/js/main.caec3c3f.chunk.js"
+  },
+  {
+    "revision": "fe6f30c4e75e94de12af",
+    "url": "/static/js/2.f75f2989.chunk.js"
+  },
+  {
+    "revision": "6ecf3a6be9e1ec0f8bce",
+    "url": "/static/css/main.d22fc4a1.chunk.css"
+  },
+  {
+    "revision": "ca635264ef03a19e0e32c5ecc25dc0d2",
+    "url": "/index.html"
+  }
+];
